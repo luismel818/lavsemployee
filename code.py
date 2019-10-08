9b6d5eb25fee7ab3077b203827cb41cff8fc4c55
luismel818 <45745495+luismel818@users.noreply.github.com>
2019-10-08 15:43:54 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+print("Welcome to your Employee Portal!")
+employees = ["0680692","1073"]
+employee_id = str(input("What is your Employee ID?"))
+if (employee_id = employees):
+  print("You have been successfully authorized to login!")
+  print("What would you like to do?")
+  print(" -  1: View my Pay Statement")
+  print(" -  2: See my Employee Information")
+  print(" -  3: Check my salary")
+  print(" -  4: Logout")
+  choice = input("Insert your Choice #")
+  if (choice = 1)
+    print("HERE IS YOUR PAY STATEMENT")
+  else: 
+    print("END OF APPLICATION.... GOODBYE....")
+    
+else: 
+  print("You are not authorized to use this application!")
